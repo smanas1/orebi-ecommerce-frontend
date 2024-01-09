bf00917ec2c8553af22eb28750c398042a0b2b40
Anas <anasolin01777@gmail.com>
2024-01-09 23:02:09 +0600

    about complete

---
+++ src/Data/aboutData.js
@@ -0,0 +1,19 @@
+export const aboutData = [
+  {
+    title: "Our Vision",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an   printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Our Story",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an   printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Our Brands",
+    des: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an   printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+];
+
+export const aboutImage = [
+  { img: "/assets/about.png" },
+  { img: "/assets/about2.png" },
+];
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,36 @@
-
+import { aboutData, aboutImage } from "../Data/aboutData";
+import Breadcrumb from "../components/layout/Breadcrumb";
 
 const About = () => {
   return (
-   <><h1>about</h1></>
+    <>
+      <div className="container font-DM">
+        <Breadcrumb
+          title="About"
+          path={window.location.pathname.split("/")[1].toUpperCase()}
+        />
+        <div className="grid grid-cols-2 gap-10">
+          {aboutImage.map((item, i) => (
+            <img key={i} src={item.img} alt="about image" />
+          ))}
+        </div>
+        <div className="my-24 ">
+          <h2 className=" text-2xl md:text-4xl max-md:text-center text-primary">
+            Orebi is one of the worlds leading ecommerce brands and is
+            internationally recognized for celebrating the essence of classic
+            Worldwide cool looking style.
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 max-md:text-center gap-10 mb-6">
+          {aboutData.map((item, i) => (
+            <div key={i}>
+              <h3 className="text-primary text-xl font-bold">{item.title}</h3>
+              <p className="text-[#767676] text-base">{item.des}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
