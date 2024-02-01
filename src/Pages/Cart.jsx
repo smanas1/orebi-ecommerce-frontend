3558aac95225d3d91c633c720da22ecaff1d8d40
Anas <anasolin01777@gmail.com>
2024-02-01 23:40:57 +0600

    cart part complete with responsive

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { cartData } from "../Data/cartData";
 import CartData from "../components/CartData";
 import Breadcrumb from "../components/layout/Breadcrumb";
@@ -12,7 +13,7 @@ const Cart = () => {
 
       {/* Cart Top Bar */}
 
-      <div className="bg-[#F5F5F3] font-bold flex justify-between p-8 border-b border-[#F5F7F7]">
+      <div className="bg-[#F5F5F3] font-bold flex text-lg justify-between p-8 border-b border-[#F5F7F7]">
         <h3 className="w-1/4">Product</h3>
         <h3>Price</h3>
         <h3>Quantity</h3>
@@ -20,7 +21,7 @@ const Cart = () => {
         <h3 className="hidden xl:block"></h3>
       </div>
       {/* Cart */}
-      <div className="border  p-5">
+      <div className="border  border-b-0">
         {/* Cart Data Map */}
         {cartData.map((item, i) => (
           <CartData
@@ -30,6 +31,47 @@ const Cart = () => {
             link={item.link}
           />
         ))}
+      </div>
+      <div className="flex border border-t-0 justify-between px-6 items-center">
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <select
+              id="countries"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pe-16 my-6"
+            >
+              <option defaultValue>SIZE</option>
+              <option value="US">SM</option>
+              <option value="CA">MD</option>
+              <option value="FR">LG</option>
+              <option value="DE">XL</option>
+            </select>
+          </div>
+          <h3 className="ms-6 font-bold text-lg">Apply coupon</h3>
+        </div>
+        <h3 className="ms-6 font-bold text-lg">Update cart</h3>
+      </div>
+      <div className="flex justify-end">
+        <div className="flex flex-col  mt-10 mb-5">
+          <div className="flex justify-end mb-5">
+            <p className="lg:text-xl text-lg font-bold ">Cart Total</p>
+          </div>
+          <div className="flex items-center border">
+            <div
+              className="py-2 ps-3 pe-10 sm:pe-52 text-lg 
+            
+            border-r font-bold "
+            >
+              Subtotal
+            </div>
+            <div className="sm:py-[11px] pe-10 sm:pe-52 ps-3 ">1000$</div>
+          </div>
+          <div className="flex items-center border border-t-0">
+            <div className=" pe-[72px] py-2 ps-3 sm:pe-60 text-lg border-r font-bold ">
+              Total
+            </div>
+            <div className="sm:py-[11px] pe-10 sm:pe-52 ps-3 ">1000$</div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/CartData.jsx
+++ src/components/CartData.jsx
@@ -1,16 +1,23 @@
 import { IoClose } from "react-icons/io5";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CartData = ({ title, price, link }) => {
   const [quantity, setQuantity] = useState(1);
+  const [total, setTotal] = useState(price);
+
+  useEffect(() => {
+    setTotal(price * quantity);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [quantity]);
 
   if (quantity === 0) {
     setQuantity(1);
   }
   return (
     <>
-      <div className="flex items-center border-b gap-3">
+      <div className="flex items-center border-b gap-3 px-5 py-1">
         <div className="flex items-center w-[42%] lg:w-[40%] lg:gap-5 gap-2">
           <IoClose size={19} />
 
@@ -20,7 +27,7 @@ const CartData = ({ title, price, link }) => {
           <h2 className="font-bold ">{title}</h2>
         </div>
         <div className="w-[17%] ">
-          <p className="lg:text-xl text-lg font-bold">{price}</p>
+          <p className="lg:text-xl text-md font-bold">{price}</p>
         </div>
         <div className="lg:w-[21%] w-[30%]">
           <form className=" -my-5 ">
@@ -34,10 +41,12 @@ const CartData = ({ title, price, link }) => {
                 id="decrement-button"
                 onClick={() => setQuantity(quantity - 1)}
                 data-input-counter-decrement="quantity-input"
-                className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                className="bg-gray-100  hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100  focus:ring-2 
+                outline-none
+                focus:outline-none"
               >
                 <svg
-                  className="w-3 h-3 text-gray-900 dark:text-white"
+                  className="w-3 h-3 text-gray-900 "
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -59,9 +68,10 @@ const CartData = ({ title, price, link }) => {
                 data-input-counter-min="1"
                 data-input-counter-max="50"
                 aria-describedby="helper-text-explanation"
-                className=" min-w-5 bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className=" min-w-5 bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500
+                outline-none block w-full py-2.5 "
                 placeholder="999"
-                value={quantity}
+                defaultValue={quantity}
                 required
               />
               <button
@@ -69,10 +79,10 @@ const CartData = ({ title, price, link }) => {
                 onClick={() => setQuantity(quantity + 1)}
                 id="increment-button"
                 data-input-counter-increment="quantity-input"
-                className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                className="bg-gray-100  hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100  focus:ring-2 focus:outline-none outline-none"
               >
                 <svg
-                  className="w-3 h-3 text-gray-900 dark:text-white"
+                  className="w-3 h-3 text-gray-900 "
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -91,7 +101,7 @@ const CartData = ({ title, price, link }) => {
           </form>
         </div>
         <div>
-          <p className="lg:text-xl text-lg font-bold">$44.00</p>
+          <p className="lg:text-xl text-md font-bold">{total}</p>
         </div>
         <div className="hidden lg:block"></div>
       </div>
